6f0246d9e09f402974554086fb923f6524d4cfa1
Emiliano Nobis <emiliano.nobis@gmail.com>
2016-08-19 20:42:35 +0200

    Pagina contatti senza form

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * @Route("/contatti", name="contacts")
+     */
+    public function contactAction(Request $request)
+    {
+        return $this->render('contacts/contacts.html.twig');
+    }
+}
