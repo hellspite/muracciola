5b040ca70a9d18f0d06f338d1713b84a2dc86456
Emiliano Nobis <emiliano.nobis@gmail.com>
2016-08-21 22:10:49 +0200

    Generato CRUD per la gallery

---
+++ src/AppBundle/Controller/PhotoController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Photo;
+use AppBundle\Form\PhotoType;
+
+/**
+ * Photo controller.
+ *
+ * @Route("/admin/gallery")
+ */
+class PhotoController extends Controller
+{
+    /**
+     * Lists all Photo entities.
+     *
+     * @Route("/", name="admin_gallery_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $photos = $em->getRepository('AppBundle:Photo')->findAll();
+
+        return $this->render('photo/index.html.twig', array(
+            'photos' => $photos,
+        ));
+    }
+
+    /**
+     * Creates a new Photo entity.
+     *
+     * @Route("/new", name="admin_gallery_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $photo = new Photo();
+        $form = $this->createForm('AppBundle\Form\PhotoType', $photo);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($photo);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_gallery_show', array('id' => $photo->getId()));
+        }
+
+        return $this->render('photo/new.html.twig', array(
+            'photo' => $photo,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Photo entity.
+     *
+     * @Route("/{id}", name="admin_gallery_show")
+     * @Method("GET")
+     */
+    public function showAction(Photo $photo)
+    {
+        $deleteForm = $this->createDeleteForm($photo);
+
+        return $this->render('photo/show.html.twig', array(
+            'photo' => $photo,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Photo entity.
+     *
+     * @Route("/{id}/edit", name="admin_gallery_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Photo $photo)
+    {
+        $deleteForm = $this->createDeleteForm($photo);
+        $editForm = $this->createForm('AppBundle\Form\PhotoType', $photo);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($photo);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_gallery_edit', array('id' => $photo->getId()));
+        }
+
+        return $this->render('photo/edit.html.twig', array(
+            'photo' => $photo,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Photo entity.
+     *
+     * @Route("/{id}", name="admin_gallery_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Photo $photo)
+    {
+        $form = $this->createDeleteForm($photo);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($photo);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_gallery_index');
+    }
+
+    /**
+     * Creates a form to delete a Photo entity.
+     *
+     * @param Photo $photo The Photo entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Photo $photo)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_gallery_delete', array('id' => $photo->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
