a51f63d829d77a741b5b8251398a07677a9c21e2
Emiliano Nobis <emiliano.nobis@gmail.com>
2016-08-21 19:56:03 +0200

    Inizio modifica stile pagina admin news edit

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -5,7 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class PostType extends AbstractType
 {
@@ -16,7 +16,9 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
+            ->add('date', DateType::class, [
+                'widget' => 'single_text' 
+            ])
             ->add('title')
             ->add('text')
         ;
