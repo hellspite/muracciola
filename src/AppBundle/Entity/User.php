9d8cd7f7fcd1d99b568ec40c6262e3747bf840d5
Emiliano Nobis <emiliano.nobis@gmail.com>
2016-08-21 16:24:21 +0200

    Generato CRUD per le news

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -46,7 +46,7 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        return ['ROLE_USER'];
+        return ['ROLE_ADMIN'];
     }
 
     public function getPassword()
